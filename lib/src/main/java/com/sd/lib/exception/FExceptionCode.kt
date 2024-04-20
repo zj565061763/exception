4820430860330b20cfc93d79fcd05d6576de70ef
zjun <565061763@qq.com>
2024-04-20 12:01:11 +0800

    FExceptionCode 异常信息描述

--- lib/src/main/java/com/sd/lib/exception/FExceptionCode.kt
+++ lib/src/main/java/com/sd/lib/exception/FExceptionCode.kt
@@ -7,4 +7,11 @@ open class FExceptionCode @JvmOverloads constructor(
     val code: Int,
     message: String? = null,
     cause: Throwable? = null,
-) : FException(message, cause)
+) : FException(message, cause) {
+
+    override val formatMessage: String
+        get() {
+            val superMessage = super.formatMessage ?: ""
+            return "${superMessage}(${code})"
+        }
+}
